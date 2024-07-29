c50b8efa02cd42d47e1a1c1633698d25f4171386
TxFig <luis.figueiredo@alluna.pt>
2024-07-29 23:56:40 +0100

    added default value for MAX_IMAGE_UPLOAD_SIZE env variable

--- src/lib/validation/book/file.ts
+++ src/lib/validation/book/file.ts
@@ -10,10 +10,12 @@ const ACCEPTED_IMAGE_TYPES = [
     "image/webp",
 ]
 
+const DEFAULT_MAX_IMAGE_UPLOAD_SIZE = 16_000_000
+
 const FileSchema = z.instanceof(File)
     .refine(
         (file) => file.size <= Number(env.PUBLIC_MAX_IMAGE_UPLOAD_SIZE),
-        `Image size can not exceeded ${prettyBytes(Number(env.PUBLIC_MAX_IMAGE_UPLOAD_SIZE))}`
+        `Image size can not exceeded ${prettyBytes(Number(env.PUBLIC_MAX_IMAGE_UPLOAD_SIZE) || DEFAULT_MAX_IMAGE_UPLOAD_SIZE)}`
     )
     .refine(
         (file) => ACCEPTED_IMAGE_TYPES.includes(file.type),
