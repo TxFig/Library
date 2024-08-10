3d8e3c427898a18dda24feb1bbe5f02eeb81468c
TxFig <luis.figueiredo@alluna.pt>
2024-08-10 11:23:48 +0100

    color logging

--- src/lib/logging/index.ts
+++ src/lib/logging/index.ts
@@ -1,12 +1,17 @@
 import { prisma } from "$lib/server/database";
 import { LogLevel } from "@prisma/client";
-
+import chalk from "chalk";
 
 
 export async function log(level: LogLevel, message: string, userId?: number, data?: unknown) {
     const metadata = JSON.stringify(data)
 
-    console.log(`${level} ${message} ${userId ?? ""} ${metadata ?? ""}`)
+    console.log(
+        chalk.blue(level),
+        chalk.green(message),
+        chalk.yellow(userId ?? ""),
+        chalk.magenta(metadata ?? "")
+    )
 
     await prisma.logEntry.create({
         data: {
