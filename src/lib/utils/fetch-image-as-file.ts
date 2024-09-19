192f5126aeb89a5a3170d13f1f063cb986bff91a
TxFig <luis.figueiredo@alluna.pt>
2024-09-19 15:11:34 +0100

    add try/catch to image fetch util function

--- src/lib/utils/fetch-image-as-file.ts
+++ src/lib/utils/fetch-image-as-file.ts
@@ -1,14 +1,20 @@
+import { logError } from "$lib/logging"
 import HttpCodes from "./http-codes"
 
 
 async function fetchImageAsFile(url: string, mimeType?: string, fetchFunction = fetch): Promise<File | null> {
-    const res = await fetchFunction(url)
-    if (res.status === HttpCodes.Success) {
-        const arrayBuffer = await res.arrayBuffer()
-        return new File([arrayBuffer], "", {
-            type: mimeType
-        })
-    } else {
+    try {
+        const res = await fetchFunction(url)
+        if (res.status === HttpCodes.Success) {
+            const arrayBuffer = await res.arrayBuffer()
+            return new File([arrayBuffer], "", {
+                type: mimeType
+            })
+        } else {
+            return null
+        }
+    } catch (err) {
+        await logError(err, `Error fetching image ${url}`)
         return null
     }
 }
