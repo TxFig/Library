7a67b1bd5d368c83ca9fee47f75d0af55cdaba69
TxFig <luis.figueiredo@alluna.pt>
2024-10-06 17:44:45 +0100

    feat(ui): ButtonWithSpinner Component

---
+++ src/lib/components/ButtonWithSpinner.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { ProgressRadial } from "@skeletonlabs/skeleton"
+
+    let loading = false
+
+    let externalClasses: string | undefined = undefined
+    export { externalClasses as class }
+</script>
+
+<button
+    class="btn variant-filled relative w-full {externalClasses}"
+    on:click={() => { loading = true }}
+    disabled={loading}
+>
+    {#if loading}
+        <ProgressRadial
+            width="w-6"
+            meter="stroke-primary-500"
+            track="stroke-primary-500/50"
+
+        />
+    {:else}
+        <slot />
+    {/if}
+</button>
